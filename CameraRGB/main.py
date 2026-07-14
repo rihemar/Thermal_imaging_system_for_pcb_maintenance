6457e4ec7f57d7f47b29f4d924322c637a81b21f
rihem <rihem.ammar@insat.ucar.tn>
2026-07-14 10:52:29 +0100

    fixed empty array

--- CameraRGB/main.py
+++ CameraRGB/main.py
@@ -7,6 +7,9 @@ from Projection import *
 
 while True:
     warped = pcb_extraction()
+    if warped is None:
+        print("No PCB detected. Please adjust the camera or the PCB position.")
+        continue
     colored_final, output = Array_processing()
     blended = Projection(warped, colored_final)
     cv2.imshow("Blended", blended)
